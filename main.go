fc4d7e8690a98004682635730cfb0ccb7baa55ef
amir gh <amir@we-pn.com>
2024-01-28 10:45:26 -0800

    feat: ui logic

---
+++ main.go
@@ -0,0 +1,392 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"image/color"
+	"log"
+	"net/url"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+type AppState struct {
+	CurrentPage string
+}
+
+type NavEvent struct {
+	TargetPage string
+}
+
+type AppSettings struct {
+	Domain string `json:"domain"`
+	DnsList string `json:"dnsList"`
+	Tcp bool `json:"tcp"`
+	Udp bool `json:"udp"`
+	ReporterURL string `json:"reporter"`
+	Configs []Config `json:"configs"`
+}
+
+type Config struct {
+	Transport string `json:"transport"`
+	TestReports []*connectivityReport `json:"testReport"`
+	Health int // 0: healthly, 1: some tests failed, 2: all tests failed
+}
+
+type AppContext struct {
+    Window      fyne.Window
+    Preferences fyne.Preferences
+    Settings   *AppSettings
+}
+
+func main() {
+	ProxyApp := app.NewWithID("com.jigsaw.proxyApp")
+	if meta := ProxyApp.Metadata(); meta.Name == "" {
+		// App not packaged, probably from `go run`.
+		meta.Name = "Proxy App"
+		app.SetMetadata(meta)
+	}
+	//ProxyApp.Settings().SetTheme(&appTheme{theme.DefaultTheme()})
+	mainWin := ProxyApp.NewWindow(ProxyApp.Metadata().Name)
+	mainWin.Resize(fyne.NewSize(400, 300))
+
+	ctx := &AppContext{
+        Window:      mainWin,
+        Preferences: ProxyApp.Preferences(),
+		Settings: &AppSettings{},
+    }
+	// Load settings from preferences
+	loadSettings(ctx)
+
+	// State variable
+	state := &AppState{CurrentPage: "main"}
+
+	// Channel for navigation events
+	navChannel := make(chan NavEvent)
+
+	// Start listening to navigation events
+	go func() {
+		for event := range navChannel {
+			state.CurrentPage = event.TargetPage
+			mainWin.SetContent(makePageContent(ctx, state, navChannel))
+		}
+	}()
+
+	// Set initial content
+	mainWin.SetContent(makePageContent(ctx, state, navChannel))
+	mainWin.ShowAndRun()
+}
+
+func loadSettings(ctx *AppContext) {
+	// Create your settings content here
+	var settings AppSettings
+	settingsJSON := ctx.Preferences.String("AppSettings")
+	if settingsJSON != "" {
+		err := json.Unmarshal([]byte(settingsJSON), &settings)
+		if err != nil {
+			log.Println("Error loading settings:", err)
+		}
+		ctx.Settings = &settings
+	}
+}
+
+func updateSettings(ctx *AppContext) {
+	// Serialize settings to JSON
+	settingsJSON, err := json.Marshal(ctx.Settings)
+	if err != nil {
+		log.Println("Error saving settings:", err)
+		return
+	}
+	// Save JSON string to app preferences
+	ctx.Preferences.SetString("AppSettings", string(settingsJSON))
+}
+
+func makePageContent(ctx *AppContext, state *AppState, navChannel chan NavEvent) fyne.CanvasObject {
+	switch state.CurrentPage {
+	case "main":
+		fmt.Println("rendering the main page")
+		return makeMainPageContent(ctx, navChannel)
+	case "settings":
+		fmt.Println("rendering the settings page")
+		return makeSettingsPageContent(ctx, navChannel)
+	// Add more cases for different pages
+	default:
+		return widget.NewLabel("Page not found")
+	}
+}
+
+// makeMainPageContent creates the main page content
+func makeMainPageContent (ctx *AppContext, navChannel chan NavEvent) fyne.CanvasObject {
+	var list *widget.List
+
+	list = widget.NewList(
+		func() int {
+			return len(ctx.Settings.Configs)
+		},
+        func() fyne.CanvasObject {
+            // Create and initialize the toolbar here
+			indicator := canvas.NewRectangle(color.Transparent)
+			indicator.SetMinSize(fyne.NewSize(10, 10))
+			label := widget.NewLabel("")
+            toolbar := widget.NewToolbar()
+
+            return container.NewHBox(
+					indicator,
+					label,
+					layout.NewSpacer(),
+					toolbar,
+			)
+        },
+        func(i widget.ListItemID, o fyne.CanvasObject) {
+            container := o.(*fyne.Container)
+			indicator := container.Objects[0].(*canvas.Rectangle)
+			label := container.Objects[1].(*widget.Label)
+            toolbar := container.Objects[3].(*widget.Toolbar)
+            
+			switch ctx.Settings.Configs[i].Health {
+			case 0:
+				indicator.FillColor = color.Transparent
+			case 1:
+				// all passed
+				indicator.FillColor = color.NRGBA{R: 0, G: 255, B: 0, A: 255}
+			case 2:
+				// some failed
+				indicator.FillColor = color.NRGBA{R: 255, G: 255, B: 0, A: 255}
+			case 3:
+				// all tests failed
+				indicator.FillColor = color.NRGBA{R: 255, G: 0, B: 0, A: 255}
+			}
+			u, err := url.Parse(ctx.Settings.Configs[i].Transport)
+			if err != nil {
+            	label.SetText("Parse error")
+			} else {
+				label.SetText(u.Host)
+			}
+			// Clear previous toolbar items and add a new delete icon
+			toolbar.Items = nil
+			
+			arrowIcon := widget.NewToolbarAction(theme.NavigateNextIcon(), func() {
+				log.Printf("Next icon clicked for item %v", i)
+				// navigate to page result for specific menu item
+				// indicator.FillColor = color.NRGBA{R: 0, G: 100, B: 0, A: 255}
+				// Define action for the "+" icon
+			})
+
+            // Create a new delete icon action for each item
+            deleteIcon := widget.NewToolbarAction(theme.DeleteIcon(), func() {
+                // Delete the item from the data slice
+                ctx.Settings.Configs = append(ctx.Settings.Configs[:i], ctx.Settings.Configs[i+1:]...)
+				updateSettings(ctx)
+                // Refresh the list to update the view
+                list.Refresh()
+
+                log.Printf("Delete icon clicked - Item %d deleted", i)
+            })
+            toolbar.Append(deleteIcon)
+			toolbar.Append(arrowIcon)
+        },
+		)
+
+	list.OnSelected = func(id widget.ListItemID) {
+		selectedItem := ctx.Settings.Configs[id].Transport
+		log.Printf("Selected Item: %s", selectedItem)
+	}
+	// list.OnUnselected = func(o fyne.CanvasObject) {
+	// }
+
+	// Create the scroll container for the list
+	scrollContainer := container.NewScroll(list)
+	// Set a minimum size for the scroll container if needed
+	scrollContainer.SetMinSize(fyne.NewSize(400, 300)) // Set width and height as needed
+	// Use container.Max to allocate as much space as possible to the list
+	listWithMaxHeight := container.NewStack(scrollContainer)
+
+
+	// Create the toolbar with a "+" icon
+	headerToolbarRight := widget.NewToolbar(
+		widget.NewToolbarAction(theme.ContentAddIcon(), func() {
+			log.Println("Add icon clicked")
+			// Define action for the "+" icon
+			c := ctx.Window.Clipboard().Content()
+			// process clipboard content
+			configURLs, err := parseInputText(c)
+			if err == nil {
+				ctx.Settings.Configs = append(ctx.Settings.Configs, configURLs...)
+				updateSettings(ctx)
+			} else {
+				log.Println("Error parsing clipboard content:", err)
+			}
+			list.Refresh()
+		}),
+	)
+
+	// Create the toolbar with settings icon
+	headerToolbarLeft := widget.NewToolbar(
+		widget.NewToolbarAction(theme.SettingsIcon(), func() {
+			log.Println("Setting icon clicked")
+			navChannel <- NavEvent{TargetPage: "settings"}
+			// myWindow.SetContent(makeSettingsPageContent())
+			// Define action for the "+" icon
+		}),
+	)
+
+	header := makePageHeader("Proxy App", headerToolbarLeft, headerToolbarRight)
+	buttonState := make(chan bool)
+	TestButton := widget.NewButton("Test All", func() {
+		go func() {
+			// Disable the button and update text in the main goroutine
+			buttonState <- true
+
+			// test all configs
+			TestConfigs(ctx.Settings)
+			log.Printf("Test reports: %v", ctx.Settings.Configs)
+			submitReports(ctx.Settings)
+			list.Refresh()
+
+			// Re-enable the button and reset text in the main goroutine
+			buttonState <- false
+		}()
+	})
+	TestButton.Importance = widget.HighImportance
+
+	go func() {
+		for update := range buttonState {
+			if update {
+				TestButton.Disable()
+				TestButton.SetText("Testing...")
+			} else {
+				TestButton.SetText("Test All")
+				TestButton.Enable()
+			}
+		}
+	}()
+	// Combine the header and the scrollable list in a vertical box layout
+	return container.NewVBox(
+		header,
+		listWithMaxHeight, // The scrollable list with enforced maximum height
+		TestButton,
+	)
+
+}
+
+// makeSettingsPageContent creates the settings page content
+func makeSettingsPageContent(ctx *AppContext, navChannel chan NavEvent) fyne.CanvasObject {
+	settings := ctx.Settings
+	domainEntry := widget.NewEntry()
+	if settings.Domain != "" {
+		domainEntry.Text = settings.Domain
+	} else {
+		domainEntry.Text = "example.com"
+	}
+	domainLabel := widget.NewRichTextFromMarkdown("**Domain**")
+
+	dnsEntry := widget.NewMultiLineEntry()
+	dnsEntry.Wrapping = fyne.TextWrapBreak
+	if settings.DnsList != "" {
+		dnsEntry.Text = settings.DnsList
+	} else {
+		dnsEntry.Text ="8.8.8.8"
+	}
+
+	dnsLabel := widget.NewRichTextFromMarkdown("**Resolvers** ([format](https://pkg.go.dev/github.com/Jigsaw-Code/outline-sdk/x/config#hdr-Config_Format))")
+
+	reporterLabel := widget.NewRichTextFromMarkdown("**Reporter URL** ([format](https://pkg.go.dev/github.com/Jigsaw-Code/outline-sdk/x/config#hdr-Config_Format))")
+	reporterEntry := widget.NewEntry()
+	reporterEntry.Text = settings.ReporterURL
+
+	checkUDP := widget.NewCheck("UDP", func(value bool) {
+		log.Println("Check set to", value)
+		settings.Udp = value
+	})
+	checkUDP.Checked = settings.Udp
+
+	checkTCP := widget.NewCheck("TCP", func(value bool) {
+		log.Println("Check set to", value)
+		settings.Udp = value
+	})
+	checkTCP.Checked = settings.Tcp
+
+	protocolSelect := container.NewHBox(
+		checkUDP,
+		checkTCP,
+	)
+
+	saveButton := widget.NewButton("Save", func() {
+		ctx.Settings.Domain = domainEntry.Text
+		ctx.Settings.DnsList = dnsEntry.Text
+		ctx.Settings.ReporterURL = reporterEntry.Text
+		ctx.Settings.Udp = checkUDP.Checked
+		ctx.Settings.Tcp = checkTCP.Checked
+		updateSettings(ctx)
+	})
+	saveButton.Importance = widget.HighImportance
+
+	// Create the toolbar with back "<-" icon
+	headerToolbarLeft := widget.NewToolbar(
+		widget.NewToolbarAction(theme.NavigateBackIcon(), func() {
+			log.Println("Setting icon clicked")
+			navChannel <- NavEvent{TargetPage: "main"}
+			// myWindow.SetContent(makeSettingsPageContent())
+			// Define action for the "+" icon
+		}),
+	)
+	// Create the toolbar with a "+" icon
+	headerToolbarRight := widget.NewToolbar()
+	header := makePageHeader("Settings", headerToolbarLeft, headerToolbarRight)
+	// Combine the header and the scrollable list in a vertical box layout
+	return container.NewVBox(
+		header,
+		domainLabel,
+		domainEntry,
+		dnsLabel,
+		dnsEntry,
+		protocolSelect,
+		reporterLabel,
+		reporterEntry,
+		saveButton,
+	)
+}
+
+func makePageHeader (title string, leftToolbar *widget.Toolbar, rightToolbar *widget.Toolbar) *fyne.Container {
+    // Create the header label
+    headerLabel := widget.NewLabel(title)
+    headerLabel.TextStyle = fyne.TextStyle{Bold: true}
+	headerLabel.Alignment = fyne.TextAlignCenter
+
+    // Create the header using HBox layout
+    header := container.NewHBox(
+		leftToolbar,
+		layout.NewSpacer(),
+        headerLabel,
+        layout.NewSpacer(), // Spacer pushes the toolbar to the right
+        rightToolbar,
+    )
+	return header
+}
+
+func parseInputText(clipboardContent string) ([]Config, error) {
+	u, err := url.Parse(clipboardContent)
+	// if parse is successful, check the schema
+	if err == nil {
+		switch u.Scheme {
+		case "ss", "socks5", "tls", "split":
+			// try to parse ss url
+			return []Config{{Transport: u.String()}}, nil
+		case "https":
+			// fetch list from remote config
+			return []Config{}, fmt.Errorf("not implemented yet")
+		case "http":
+			// reject url due to security issue
+			return []Config{}, fmt.Errorf("not implemented yet")
+		default:
+			// reject url due to unknown schema
+			return []Config{}, fmt.Errorf("not implemented yet")
+		}
+	}
+	return []Config{}, fmt.Errorf("failed to parse input")	
+}
